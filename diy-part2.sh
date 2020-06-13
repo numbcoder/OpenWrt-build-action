fa5393cb0be7a6450ed5be8342c0a457b93fe890
Seekr <wzhao23@gmail.com>
2020-06-14 02:14:08 +0800

    add config

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,3 +10,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Add packages
+git clone https://github.com/vernesong/OpenClash package/openclash
